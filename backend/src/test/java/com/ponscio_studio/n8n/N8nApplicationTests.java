b4a2a5cea05b5398c9e152599f2513536fe47c6a
pato <abuelardoenmispina@acitromicina.com>
2026-03-09 12:33:32 -0500

    feat: :sparkles: My first Commit!

---
+++ backend/src/test/java/com/ponscio_studio/n8n/N8nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponscio_studio.n8n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N8nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
