10ce4123638b85f815de295e63fc5efaf6c0fd05
Juan Diego C <juanchocristancho2009@gmail.com>
2026-03-12 11:25:10 -0500

    feat: :sparkles: Cords

--- backend/src/main/java/com/ponscio_studio/n8n/N8nApplication.java
+++ backend/src/main/java/com/ponscio_studio/n8n/N8nApplication.java
@@ -2,6 +2,9 @@ package com.ponscio_studio.n8n;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class N8nApplication {
@@ -10,4 +13,15 @@ public class N8nApplication {
 		SpringApplication.run(N8nApplication.class, args);
 	}
 
+
+	@Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500","http://localhost",
+                "http://localhost:8080").allowedMethods("*").allowedHeaders("*").allowedOriginPatterns("*");
+            }
+        };
+    }
 }
--- backend/src/main/java/com/ponscio_studio/n8n/applicaction/useCase/GetWeatherUseCase.java
+++ backend/src/main/java/com/ponscio_studio/n8n/applicaction/useCase/GetWeatherUseCase.java
@@ -24,5 +24,4 @@ public class GetWeatherUseCase {
         
         return body;
     }
-
 }
